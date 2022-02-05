4b28fa29d28643cf7e9a997bd12d16be0240e868
G1A021070_FREGY RESE RIO SANDIKA <99086240+resekk@users.noreply.github.com>
2022-02-05 22:52:43 +0700

    Add files via upload

---
+++ fregy.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int p,l,keliling;
+
+    cout<<"masukan panjang persegi panjang : ";
+    cin>>p;
+    cout<<"masukan lebar persegi panjang : ";
+    cin>>l;
+
+    keliling=p+l+p+l;
+    cout<<"keliling persegi panjang adalah "<<keliling;
+    return 0;
+}
